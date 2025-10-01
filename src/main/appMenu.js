ed88c39a5d01e1c2d575bd7d795102843743444a
dsrtartweb10-prog <dsrtartweb10@gmail.com>
2025-10-02 04:55:20 +0700

    Create appMenu.js

---
+++ src/main/appMenu.js
@@ -0,0 +1,30 @@
+const { Menu } = require("electron");
+
+function setupMenu(win) {
+  const template = [
+    {
+      label: "File",
+      submenu: [
+        { role: "quit" }
+      ],
+    },
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "toggleDevTools" },
+        { type: "separator" },
+        { role: "resetZoom" },
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { type: "separator" },
+        { role: "togglefullscreen" },
+      ],
+    },
+  ];
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
+module.exports = { setupMenu };
